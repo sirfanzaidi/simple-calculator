f253c87158b6cdc19073b39f3c2c9b4520801513
IrfanZaidi <irfanhussain2@gmail.com>
2024-03-20 16:44:45 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+const answer = await inquirer.prompt([
+  { massage: "enter first number", type: "number", name: "firstNumber" },
+  { massage: "enter second number", type: "number", name: "secondNumber" },
+  {
+    massage: "select one of the operators from action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtration", "Multiplication", "Division"],
+  },
+]);
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtration") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("please select valid operator");
+}echo "# simple-calculator" >> README.md
